a63e0f940e8dfeb3a6e166d1d15171c863bf0c92
stella-Yuchongxi <chongxi_yu18@163.com>
2025-09-20 19:00:07 +1000

    Initial commit: separated frontend and backend

---
+++ backend/Leetcode136.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for num in nums:
+            result ^= num   # 异或：相同为0，不同为1
+        return result
